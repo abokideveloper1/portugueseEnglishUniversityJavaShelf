ac3adb968f27fb2f1fb68a55de3b2dee57392f3a
abokideveloper1 <xandykelvin@gmail.com>
2021-02-18 22:20:28 -0300

    A new edit was done to correct errors

--- src/application/trimesterSpenditure.java
+++ src/application/trimesterSpenditure.java
@@ -7,12 +7,12 @@ public class trimesterSpenditure {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter total amount of money spent during January: ");
 		float january = input.nextFloat();
-		System.out.print("Enter total amount of money spent during February");
+		System.out.print("Enter total amount of money spent during February: ");
 		float february = input.nextFloat();
 		System.out.print("Enter total amount of money spent during March: ");
 		float march = input.nextFloat();
 		
-		float trimester = input.nextFloat();
+		float trimester = january + february + march;
 		
 		System.out.println(" ");
 		System.out.println("TRIMESTER TOTAL EXPENDITURE: "+trimester);
